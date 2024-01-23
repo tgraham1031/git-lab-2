ffd4aa2ced4c37fc28ab7b8452a599395c6c33e5
tgraham1031 <tg169922@ohio.edu>
2024-01-23 12:05:34 -0500

    New program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Taryn Graham
+ *        Date: 1/23/2024
+ * Description: This program is for Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
